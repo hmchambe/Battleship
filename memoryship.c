f6b081988441e8218bf5878e016dd027ef29b173
hmchambe <hmchambe@mtu.edu>
2019-04-09 18:08:34 -0400

    Initial Commit

---
+++ memoryship.c
@@ -0,0 +1,9 @@
+/*
+ * @author: Hunter Chambers
+ *
+ * This is Memoryship based on battleship
+ * Each player will have a 5x5 grid area
+ * Each ship is a single cell
+ * Each side has 5 ships
+ *
+ */
